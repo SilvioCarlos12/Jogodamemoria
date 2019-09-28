d5ea0c5ae148ee7d6bd5c3e4bff9d48b58ee02e5
silvio <silvio.net.ma@gmail.com>
2019-09-28 20:51:40 -0300

    subindo os diretorios

---
+++ js/logica.js
@@ -0,0 +1,9 @@
+var aleatorio;
+var imagem = ["Papel", "Pedra", "Tesoura"];
+
+for (cont = 0; cont < 3; cont++) {
+    $("#imagem").append(`<img class="imagem" src="img/${imagem[cont]}.png">`);
+
+    $("#imagem").append(`<img class="imagem" src="img/${imagem[cont]}.png">`);
+
+}
