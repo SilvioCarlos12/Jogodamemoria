e22627bff8a4d31b2a9cdb4af57781613406882c
silvio <silvio.net.ma@gmail.com>
2019-09-30 14:19:16 -0300

    colacando as imagens na telajogo e ajeitando os caminho na telajogo

--- js/logica.js
+++ js/logica.js
@@ -2,8 +2,9 @@ var aleatorio;
 var imagem = ["Papel", "Pedra", "Tesoura"];
 
 for (cont = 0; cont < 3; cont++) {
-    $("#imagem").append(`<img class="imagem" src="img/${imagem[cont]}.png">`);
+    aleatorio = Math.floor(Math.random() * imagem.length);
+    $("#imagem").append(`<img class="imagem" src="/img/${imagem[aleatorio]}.png">`);
 
-    $("#imagem").append(`<img class="imagem" src="img/${imagem[cont]}.png">`);
+    $("#imagem").append(`<img class="imagem" src="/img/${imagem[aleatorio]}.png">`);
 
 }
