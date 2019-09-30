8f20af4ff4321bfc5df61ae6b408e945cb8fa1e3
silvio <silvio.net.ma@gmail.com>
2019-09-30 14:32:34 -0300

    adicionando na pasta css styletelainicio e styletelajogo

--- js/logica.js
+++ js/logica.js
@@ -1,7 +1,7 @@
 var aleatorio;
 var imagem = ["Papel", "Pedra", "Tesoura"];
 
-for (cont = 0; cont < 3; cont++) {
+for (cont = 0; cont < imagem.length; cont++) {
     aleatorio = Math.floor(Math.random() * imagem.length);
     $("#imagem").append(`<img class="imagem" src="/img/${imagem[aleatorio]}.png">`);
 
